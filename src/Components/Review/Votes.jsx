1e75b9accbf3d477bfd8594f11f5573e2e91e914
RichardNeat <rneat@hotmail.co.uk>
2022-08-22 17:54:13 +0100

    like button functionality added to undo

--- src/Components/Review/Votes.jsx
+++ src/Components/Review/Votes.jsx
@@ -5,40 +5,69 @@ export default function Votes ({id, votes}) {
 
     const [voteTally, setVoteTally] = useState(votes);
     const [err, setErr] = useState(null);
+    const [upClicks, setUpClicks] = useState(0);
+    const [downClicks, setDownClicks] = useState(0);
+    const [upDisabled, setUpDisabled] = useState(false);
+    const [downDisabled, setDownDisabled] = useState(false);
 
     const handleUpClick = () => {
-        setVoteTally((currVoteTally) => {
-            return currVoteTally + 1;
-        });
-        addVote(id, 1).catch(() => {
+        if (upClicks === 0) {
+            setVoteTally((currVoteTally) => {
+                return currVoteTally + 1;
+            })
+            setDownDisabled(true);
+            setUpClicks(1);
+            setErr(null);
+            addVote(id, 1).catch(() => {
+                setVoteTally((currVoteTally) => {
+                    return currVoteTally - 1;
+                });
+                setErr('Something went wrong, please try again');
+            });
+        } else {
             setVoteTally((currVoteTally) => {
                 return currVoteTally - 1;
-            });
-            setErr('Something went wrong, please try again');
-        });
+            })
+            setDownDisabled(false);
+            setUpClicks(0);
+            setErr(null);
+            addVote(id, -1)
+        };
     };
 
     const handleDownClick = () => {
-        setVoteTally((currVoteTally) => {
-            return currVoteTally - 1;
-        });
-        addVote(id, -1).catch(() => {
+        if (downClicks === 0) {
+            setVoteTally((currVoteTally) => {
+                return currVoteTally - 1;
+            });
+            setUpDisabled(true);
+            setDownClicks(1);
+            setErr(null);
+            addVote(id, -1).catch(() => {
+                setVoteTally((currVoteTally) => {
+                    return currVoteTally + 1;
+                });
+                setErr('Something went wrong, please try again');
+            });
+        } else {
             setVoteTally((currVoteTally) => {
                 return currVoteTally + 1;
-            });
-            setErr('Something went wrong, please try again');
-        });
+            })
+            setUpDisabled(false)
+            setDownClicks(0);
+            setErr(null);
+            addVote(id, 1);
+        };
     };
-
-    if (err) return <p>{err}</p>
     
     return (
         <>
             <p className="review-votes">
             Votes: {voteTally}
-            <button onClick={handleUpClick} className="vote-button"><strong>⬆</strong></button>
-            <button onClick={handleDownClick} className="downVote-button"><strong>⬇</strong></button>
+            <button disabled={upDisabled} onClick={handleUpClick} className="upVote-button"><strong>{upClicks ? "undo": "⬆"}</strong></button>
+            <button disabled={downDisabled} onClick={handleDownClick} className="downVote-button"><strong>{downClicks ? "undo": "⬇"}</strong></button>
+            {err ? <p>{err}</p>: ''}
             </p>
         </>
-    )
-}
+    );
+};
