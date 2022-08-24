c657946101cc7c44449cd82ad1fe4915ce2248fe
RichardNeat <rneat@hotmail.co.uk>
2022-08-24 12:21:16 +0100

    added button disabled on click

--- src/Components/Review/Queries/DeleteComment.jsx
+++ src/Components/Review/Queries/DeleteComment.jsx
@@ -3,26 +3,31 @@ import { deleteComment } from "../../../apis";
 
 export default function DeleteComment ({id, setDeleted, setSuccess}) {
 
+    // implement functionality where user can only delete their own comment once users has been added
+
     const [isLoading, setIsLoading] = useState(false);
     const [err, setErr] = useState(false);
+    const [buttonDisabled, setButtonDisabled] = useState(false);
 
     const handleClick = () => {
+        setButtonDisabled(true);
         setSuccess(false);
         setDeleted(false);
         setIsLoading(true);
         deleteComment(id).then(() => {
-            setIsLoading(false)
-            setDeleted(true)
+            setIsLoading(false);
+            setDeleted(true);
         })
         .catch(() => {
             setIsLoading(false);
             setErr(true);
+            setButtonDisabled(false);
         });
     };
 
     return (
         <>
-            <button onClick={handleClick}>Delete</button>
+            <button disabled={buttonDisabled ? true: false} onClick={handleClick}>Delete</button>
             {isLoading ? <p>Deleting...</p>: null}
             {err ? <p>Something went wrong please try again</p>: null}
         </>
