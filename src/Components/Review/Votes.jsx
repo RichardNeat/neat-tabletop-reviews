5c52631b111125caf9a27ab3c6631e901b28ebac
RichardNeat <rneat@hotmail.co.uk>
2022-08-26 08:45:31 +0100

    users can only vote when logged in

--- src/Components/Review/Votes.jsx
+++ src/Components/Review/Votes.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import { addVote } from "../../apis"
+import { addVote } from "../../apis";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/current-user";
+import { Link } from "react-router-dom";
 
 export default function Votes ({id, votes, comment_id, comment_votes}) {
+
+    const {currUser, setCurrUser} = useContext(UserContext);
 
     // console.log(id, votes, comment_id, comment_votes)
 
@@ -11,64 +16,73 @@ export default function Votes ({id, votes, comment_id, comment_votes}) {
     const [downClicks, setDownClicks] = useState(0);
     const [upDisabled, setUpDisabled] = useState(false);
     const [downDisabled, setDownDisabled] = useState(false);
+    const [guest, setGuest] = useState(false);
 
     const handleUpClick = () => {
-        if (upClicks === 0) {
-            setVoteTally((currVoteTally) => {
-                return currVoteTally + 1;
-            })
-            setDownDisabled(true);
-            setUpClicks(1);
-            setErr(null);
-            addVote(id, 1, comment_id).catch(() => {
+        if (currUser !== 'guest') {
+            if (upClicks === 0) {
+                setVoteTally((currVoteTally) => {
+                    return currVoteTally + 1;
+                })
+                setDownDisabled(true);
+                setUpClicks(1);
+                setErr(null);
+                addVote(id, 1, comment_id).catch(() => {
+                    setVoteTally((currVoteTally) => {
+                        return currVoteTally - 1;
+                    });
+                    setErr('Something went wrong, please try again');
+                });
+            } else {
                 setVoteTally((currVoteTally) => {
                     return currVoteTally - 1;
+                })
+                setDownDisabled(false);
+                setUpClicks(0);
+                setErr(null);
+                addVote(id, -1, comment_id).catch(() => {
+                    setVoteTally((currVoteTally) => {
+                        return currVoteTally - 1;
+                    });
+                    setErr('Something went wrong, please try again');
                 });
-                setErr('Something went wrong, please try again');
-            });
+            };
         } else {
-            setVoteTally((currVoteTally) => {
-                return currVoteTally - 1;
-            })
-            setDownDisabled(false);
-            setUpClicks(0);
-            setErr(null);
-            addVote(id, -1, comment_id).catch(() => {
-                setVoteTally((currVoteTally) => {
-                    return currVoteTally - 1;
-                });
-                setErr('Something went wrong, please try again');
-            });
+            setGuest(true);
         };
     };
 
     const handleDownClick = () => {
-        if (downClicks === 0) {
-            setVoteTally((currVoteTally) => {
-                return currVoteTally - 1;
-            });
-            setUpDisabled(true);
-            setDownClicks(1);
-            setErr(null);
-            addVote(id, -1, comment_id).catch(() => {
-                setVoteTally((currVoteTally) => {
-                    return currVoteTally + 1;
-                });
-                setErr('Something went wrong, please try again');
-            });
-        } else {
-            setVoteTally((currVoteTally) => {
-                return currVoteTally + 1;
-            })
-            setUpDisabled(false)
-            setDownClicks(0);
-            setErr(null);
-            addVote(id, 1, comment_id).catch(() => {
+        if (currUser !== 'guest') {
+            if (downClicks === 0) {
                 setVoteTally((currVoteTally) => {
                     return currVoteTally - 1;
                 });
-                setErr('Something went wrong, please try again');
-            });
+                setUpDisabled(true);
+                setDownClicks(1);
+                setErr(null);
+                addVote(id, -1, comment_id).catch(() => {
+                    setVoteTally((currVoteTally) => {
+                        return currVoteTally + 1;
+                    });
+                    setErr('Something went wrong, please try again');
+                });
+            } else {
+                setVoteTally((currVoteTally) => {
+                    return currVoteTally + 1;
+                })
+                setUpDisabled(false)
+                setDownClicks(0);
+                setErr(null);
+                addVote(id, 1, comment_id).catch(() => {
+                    setVoteTally((currVoteTally) => {
+                        return currVoteTally - 1;
+                    });
+                    setErr('Something went wrong, please try again');
+                });
+            };
+        } else {
+            setGuest(true);
         };
     };
     
@@ -79,6 +93,8 @@ export default function Votes ({id, votes, comment_id, comment_votes}) {
             <button disabled={upDisabled} onClick={handleUpClick} className="upVote-button"><strong>{upClicks ? "undo": "⬆"}</strong></button>
             <button disabled={downDisabled} onClick={handleDownClick} className="downVote-button"><strong>{downClicks ? "undo": "⬇"}</strong></button>
             {err ? <p>{err}</p>: null}
+            {guest ? <p>You must be logged in to add vote!</p>: null}
+            {guest ? <Link to={'/users'}>Login here!</Link>: null}
             </p>
         </>
     );
